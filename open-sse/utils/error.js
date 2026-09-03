askidmobile/9router

fix(fallback): fill resetsAtMs via generic parser when executor parse lacks it
Base executor parseError returns {status, message} and short-circuits
parseUpstreamError before the generic 429 reset-time extraction ran —
GLM's "reset at <datetime>" never reached the lock. Now the executor
result wins, but resetsAtMs falls back to the generic parser.

open-sse/utils/error.js
@@ -108,7 +108,9 @@ export async function parseUpstreamError(response, executor = null) {
       const parsed = executor.parseError(response, bodyText);
       if (parsed && typeof parsed === "object") {
         const msg = parsed.message || DEFAULT_ERROR_MESSAGES[response.status] || `Upstream error: ${response.status}`;
-        return { statusCode: parsed.status || response.status, message: msg, resetsAtMs: parsed.resetsAtMs };
+        // Executor parse wins; fill resetsAtMs from generic patterns when absent
+        const resetsAtMs = parsed.resetsAtMs ?? (response.status === 429 ? extractResetsAtMs(response, msg) : null);
+        return { statusCode: parsed.status || response.status, message: msg, resetsAtMs };
       }
     } catch { /* fall through to default parsing */ }
   }

golamrabbi696/EzRouter

open-sse/utils/error.js
@@ -103,10 +103,11 @@ export async function parseUpstreamError(response, executor = null) {
         const message = parsed.message && parsed.message !== bodyText
           ? parsed.message
           : fallbackMessage;
+        const resetsAtMs = parsed.resetsAtMs ?? (response.status === 429 ? extractResetsAtMs(response, message) : null);
         return {
           statusCode: parsed.status || response.status,
           message,
-          resetsAtMs: parsed.resetsAtMs,
+          resetsAtMs,
           code: parsed.code ?? structuredCode,
         };
       }